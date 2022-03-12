4897e83567d8083d9f73109ad552b9032527f021
Erbol94 <erbolshakeev@gmail.com>
2022-03-12 19:36:25 +0600

    test

---
+++ Lesson3/dz/test.js
@@ -0,0 +1,30 @@
+// function number(num1, num2) {
+//
+//     if(num1 < num2){
+//         alert(num1 + 'меньше чем ' + num2)
+//     }
+//     if (num1 > num2){
+//         alert(num1 + 'больше чем ' + num2)
+//     }
+// }
+// var num1 = prompt('Первое число');
+// var num2 = prompt('Второе число');
+// number(num1, num2);
+
+// function twoMass(array1, array2) {
+// //     if(array1 < array2){
+// //         alert(array1.length +' меньше '+ array2.length)
+// //     }
+// //     if (array1 > array2){
+// //         alert(array1.length + ' больше ' +array2.length)
+// //     }
+// // }
+// // var array1 = prompt('First');
+// // var array2 = prompt('Second');
+// // twoMass(array1,array2);
+
+function countChar(first) {
+        alert(first.length)
+}
+let first = [1, 2, 3, 4, 5];
+countChar(first);
---
+++ Lesson3/main.js
@@ -0,0 +1,48 @@
+const a= '1';
+let b= 2;
+
+for (let ker of[]){
+    console.log(key)
+}
+
+while ( 1 < 0 ){
+
+}
+
+if(a === b){
+
+}
+
+// function sayHello() {
+//     console.log("Hello!")
+// }
+// sayHello();
+
+function getFullName(user, age) {
+    if(age < 18 ){
+        console.log("Close")
+    }else {
+        console.log("Open")
+    }
+}
+
+const user = {
+    first_name: "John",
+    last_name: "kia"
+}
+const user2 = {
+    first_name: 'Aktan',
+    last_name: 'Batman'
+}
+const user3 = {
+    first_name: 'Atabek',
+    last_name: 'Super_rovno'
+}
+getFullName(user.first_name, 23);
+getFullName(user2, 16);
+getFullName(user3, 15);
+
+// console.log(user.first_name, user.last_name);
+// console.log(user2.first_name, user2.last_name);
+// console.log(user3.first_name, user3.last_name);
+
